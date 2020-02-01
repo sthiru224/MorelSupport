62d6639fcb4a851536758d6987ad2d1e0c232f6a
sthiru224 <60024722+sthiru224@users.noreply.github.com>
2020-02-01 13:49:23 -0800

    move left and right and it works!

---
+++ Morel_Support_Unity/Assets/move_horizontal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_left : MonoBehaviour
+{ 
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(Input.GetAxis("Horizontal") *15f* Time.deltaTime, 0f, 0f);
+    }
+
+    
+}
