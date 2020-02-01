c66bd06fa1c0786405400d890cbefac9be555d5b
sthiru224 <60024722+sthiru224@users.noreply.github.com>
2020-02-01 14:43:05 -0800

    can move up down right left and honestly any direction atp

--- Morel_Support_Unity/Assets/move_horizontal.cs
+++ Morel_Support_Unity/Assets/move_horizontal.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move_left : MonoBehaviour
+public class move_horizontal : MonoBehaviour
 { 
 
     // Update is called once per frame
--- Morel_Support_Unity/Assets/move_vertical.cs
+++ Morel_Support_Unity/Assets/move_vertical.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class move_vertical : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float moveSpeed = 15f;
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
-        
-    }
+		transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed) ;
+	}
+
+
+
 }
