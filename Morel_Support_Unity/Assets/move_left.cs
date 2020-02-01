e585fd7db504354950f6ccdaa267b938dc5a2dc0
Michel <60534295+acrystalbirdie@users.noreply.github.com>
2020-01-31 22:46:43 -0800

    started script for move_left

---
+++ Morel_Support_Unity/Assets/move_left.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_left : MonoBehaviour
+{
+    float moveSpeed;
+    RigidBody2D rigidbody2d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    rigidbody2d = GetComponent<RigidBody2D>();
+    moveSpeed = 10f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (Input.GetKey(KeyCode.A))
+
+        rigidbody2d.velocity = transform.right * moveSpeed;
+        
+    }
+    
+    
+}
