8e300748ec4f9612a11d8178dc54f004aa98e773
sthiru224 <60024722+sthiru224@users.noreply.github.com>
2020-02-02 13:21:40 -0800

    updated to switchsprite and logchanger

---
+++ Morel_Support_Unity/Assets/logchanges.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class logchanges : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public Sprite Alive;
+	public Sprite Dead;
+
+	public bool Inside = false;
+
+
+
+	private void OnTriggerEnter2D()
+	{
+		Debug.Log("hehyyyyyy");
+
+		Inside = true;
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.F) && Inside == true)
+		{
+			Debug.Log("is workkk");
+			if (this.gameObject.GetComponent<SpriteRenderer>().sprite = Alive)
+			{
+				this.gameObject.GetComponent<SpriteRenderer>().sprite = Dead;
+			}
+		}
+	}
+
+}
