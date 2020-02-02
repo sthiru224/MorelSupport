84c66dab833b5454de8971243f8ee31b07fb07d3
Michel <60534295+acrystalbirdie@users.noreply.github.com>
2020-02-02 13:13:24 -0800

    updated master to sync

---
+++ Morel_Support_Unity/Assets/spritechange.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spritechange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite Idle;
+    public Sprite Run;
+
+
+
+    void Update()
+
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite = Idle)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Run;
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite = Idle)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Run;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite = Idle)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Run;
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite = Idle)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Run;
+            }
+        }
+
+
+    }
+
+}
