8e26a27890a0ab0a5429e31f65c68e88a62e299f
Michel <60534295+acrystalbirdie@users.noreply.github.com>
2020-02-01 16:39:34 -0800

    added physics-based motion

--- Morel_Support_Unity/Assets/move_horizontal.cs
+++ Morel_Support_Unity/Assets/move_horizontal.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class move_horizontal : MonoBehaviour
-{ 
+{
+	public float moveSpeed = 15f;
+	bool moving = false;
+	private float t = 0.0f;
 
-    // Update is called once per frame
-    void Update()
+	private Rigidbody2D rBody;
+
+	void Awake()
+	{
+		rBody = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-		transform.Translate(Input.GetAxis("Horizontal") *15f* Time.deltaTime, 0f, 0f);
-    }
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rBody.velocity = new Vector2(-1.0f, 0.0f);
+            moving = true;
+            t = 0.0f;
+        }
+    if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rBody.velocity = new Vector2(1.0f, 0.0f);
+            moving = true;
+            t = 0.0f;
+        }
+    if (moving)
+        {
+            // Record the time spent moving up or down.
+            // When this is 1sec then display info
+            t = t + Time.deltaTime;
+            if (t > 1.0f)
+            {
+                Debug.Log(gameObject.transform.position.y + " : " + t);
+                t = 0.0f;
+            }
+        }
+        
+		//rBody.AddForce(Vector2.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+	}
 
-    
+
+
 }
--- Morel_Support_Unity/Assets/move_vertical.cs
+++ Morel_Support_Unity/Assets/move_vertical.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class move_vertical : MonoBehaviour
 {
 	public float moveSpeed = 15f;
+	bool moving = false;
+	private float t = 0.0f;
+
+	private Rigidbody2D rBody;
+
+	void Awake()
+	{
+		rBody = GetComponent<Rigidbody2D>();
+	}
 
 	// Update is called once per frame
 	void Update()
     {
-		transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed) ;
+		if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rBody.velocity = new Vector2(0.0f, 2.0f);
+            moving = true;
+            t = 0.0f;
+        }
+		//rBody.AddForce(Vector2.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
 	}
 
 
